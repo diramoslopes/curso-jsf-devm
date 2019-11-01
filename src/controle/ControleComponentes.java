2db2e1e3310b8c7bbbf5d8cf6c186c1fec2fe022
Diogo <dramoslopesilveira@gmail.com>
2019-11-01 15:39:03 -0300

    componentes botoes e links

--- src/controle/ControleComponentes.java
+++ src/controle/ControleComponentes.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.event.ActionEvent;
 
 @ManagedBean(name = "controleComponentes")
 @SessionScoped
 public class ControleComponentes implements Serializable{
 	
 	private String texto;
+	private String idComponente;
 	
 	public ControleComponentes() {
 	}
@@ -17,6 +20,15 @@ public class ControleComponentes implements Serializable{
 	public String geraTexto() {
 		texto = "texto gerado: < > & <b>Texto em negrito</b>";
 		return "componentesParte2";
+	}
+	
+	public void listerner(ActionEvent event) {
+		UIComponent source = event.getComponent();
+		idComponente = "Componente que executou a ação: " +  source.getId();
+	}
+	
+	public String executar() {
+		return "componentesParte3";
 	}
 
 	public String getTexto() {
@@ -27,4 +39,12 @@ public class ControleComponentes implements Serializable{
 		this.texto = texto;
 	}
 
+	public String getIdComponente() {
+		return idComponente;
+	}
+
+	public void setIdComponente(String idComponente) {
+		this.idComponente = idComponente;
+	}
+	
 }
